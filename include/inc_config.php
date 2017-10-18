82f9c838edc05b09f57cfe23e9151d7f35bf62fd
Steven Coates <Steven2.Coates@live.uwe.ac.uk>
2017-10-18 10:58:25 +0100

    Tutorial work

--- include/inc_config.php
+++ include/inc_config.php
@@ -3,5 +3,11 @@
 $configuration = simplexml_load_file("configuration.xml");
 
 define("BASE_CURRENCY", $configuration->base);
-define("CURRENCIES_FILE", $configuration->currencies);
-define("RATES_FILE", $configuration->rates);
+
+define("CURRENCIES_FILE", $configuration->data->currencies);
+define("CURRENCIES_SOURCE", $configuration->sources->currencies);
+
+define("RATES_FILE", $configuration->data->rates);
+define("RATES_SOURCE", $configuration->sources->rates);
+
+define("CURRENCIES", explode(", ", $configuration->currencies));
--- include/inc_rates.php
+++ include/inc_rates.php
@@ -2,27 +2,34 @@
 
 /**
  * Description of inc_rates
- *
- * @author steve
  */
 class rates {
-	private $data;
+	private $currencies;
+	private $rates;
 	
 	function __construct() {
-		$this->data = new DOMDocument();
-		if(file_exists(RATES_FILE)) {
-			$this->populate_rates();
+		$this->currencies = new DOMDocument();
+		$this->rates = new DOMDocument();
+		
+		//If the currencies file does not exist, populate it before loading
+		if(file_exists(CURRENCIES_FILE)) {
+			$this->initialise_currencies();
 		}
-		else {
+		$this->currencies->load(CURRENCIES_FILE);
+		
+		//If the rates file does not exist, populate it befor eloading
+		if(file_exists(RATES_FILE)) {
 			$this->initialise_rates();
 		}
+		$this->rates->load(RATES_FILE);
 	}
     
-	private function initialise_rates() {
+	private function initialise_currencies() {
 		$rawCurrencies = new DOMDocument();
-		$rawCurrencies->load(CURRENCIES_FILE);
+		$rawCurrencies->load(CURRENCIES_SOURCE);
 		
 		$currencies = array();
+		//First process the raw data so that it is grouped by currency, not country
 		foreach($rawCurrencies->getElementsByTagName("CcyNtry") AS $currency) {
 			//Check if a currency has yet been processed with this code
 			if(!isset($currencies[$currency->getElementsByTagName("Ccy")->item(0)->nodeValue])) {
@@ -38,16 +45,30 @@ class rates {
 			$currencies[$currency->getElementsByTagName("Ccy")->item(0)->nodeValue]['loc'][] =
 				$currency->getElementsByTagName("CtryNm")->item(0)->nodeValue;
 		}
-		echo "<pre>";
-		print_r($currencies);
-		echo "</pre>";
+		
+		//Piece together the XML file to be saved with all currencies
+		$xml =
+		"<?xml version='1.0' encoding='UTF-8'?>".
+		"<currencies>";
+			foreach($currencies AS $currency) {
+				$xml .=
+				"<currency>".
+					"<code>{$currency['code']}</code>".
+					"<name>{$currency['curr']}</name>".
+					//Locaitons need joining, since stored as an array
+					"<location>".join(", ", $currency['loc'])."</location>".
+				"</currency>";
+			}
+			$xml .=
+		"</currencies>";
+			
+		file_put_contents(CURRENCIES_FILE, $xml);
 	}
 	
-	private function populate_rates() {
-		
-	}
-	
-	private function save_rates() {
-		
+	private function initialise_rates() {
+		$yql = "";
+		foreach(CURRENCIES AS $currency) {
+			
+		}
 	}
 }
